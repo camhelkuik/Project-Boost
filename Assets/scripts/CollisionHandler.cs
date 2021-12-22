1018a6a89a5f397cb1dcacbd3b5e7a5a9adb66ea
Cameron Helkuik <cameronhelkuik@gmail.com>
2021-12-21 19:35:58 -0600

    Audio is working and so is reload scene

---
+++ Assets/scripts/CollisionHandler.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Hit a friendly thing");
+                break;
+            case "Finish":
+                Debug.Log("You did it, you finished!");
+                break;
+            case "Fuel":
+                Debug.Log("Add fuel!!");
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }
+    }
+
+    private void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
