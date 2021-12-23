7a943a4fbb05c5504551ee256cd9531d3d04d86d
Cameron Helkuik <cameronhelkuik@gmail.com>
2021-12-23 16:31:53 -0600

    made new scene, loading scenes

--- Assets/scripts/CollisionHandler.cs
+++ Assets/scripts/CollisionHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,7 +14,7 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Hit a friendly thing");
                 break;
             case "Finish":
-                Debug.Log("You did it, you finished!");
+                LoadNextLevel();
                 break;
             case "Fuel":
                 Debug.Log("Add fuel!!");
@@ -24,9 +25,23 @@ public class CollisionHandler : MonoBehaviour
         }
     }
 
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
     private void ReloadLevel()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
     }
+
 }
